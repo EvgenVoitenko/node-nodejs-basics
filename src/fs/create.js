d1eeb4ba18342037bfaf18b1908816cb6f080622
EvgenVoitenko <evgenvoitenko@gmail.com>
2022-11-29 15:55:25 +0300

    feat: add solution fs/create

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,25 @@
+import * as fs from 'node:fs/promises';
+import * as path from 'node:path';
+
+
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const create = async () => {
-    // Write your code here 
+    let a = 0;
+    try {
+        await fs.access(path.join(__dirname, "files", 'fresh.txt'));
+
+
+    } catch (err) {
+        a = 1;
+    }
+    if (a == 1) {
+        await fs.writeFile((path.join(__dirname, "files", 'fresh.txt')), 'I am fresh and young')
+    }
+    if (a == 0) { throw new Error('FS operation failed'); }
 };
 
 await create();
